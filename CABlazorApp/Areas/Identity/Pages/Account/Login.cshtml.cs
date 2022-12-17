1b07a5ef0bf460cb965253903755c1733beb69e6
dortwess <dortwess@gmail.com>
2022-12-17 15:44:38 +0100

    working

--- CABlazorApp/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ CABlazorApp/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -72,7 +72,7 @@ namespace CABlazorApp.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
+            [Required(ErrorMessage = "Heslo je potřeba.")]
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
