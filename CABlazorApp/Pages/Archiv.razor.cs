6784b11ece070013b9142f90f111d89f755906b5
dortwess <dortwess@gmail.com>
2023-01-04 14:23:56 +0100

    working

--- CABlazorApp/Pages/Archiv.razor.cs
+++ CABlazorApp/Pages/Archiv.razor.cs
@@ -8,34 +8,8 @@ namespace CABlazorApp.Pages;
 
 public class Archiv2 : PageModel
 {
-    [Inject]
-    public IWebHostEnvironment Environment { get; set; }
-    
-    [Inject]
-    public AuthenticationStateProvider GetAuthenticationStateAsync { get; set; }
-
-    public async Task<string> UserLoad()
+    private void OnGet()
     {
-        var authstate = await GetAuthenticationStateAsync.GetAuthenticationStateAsync();
-        var user = authstate.User;
-        var username = user.Identity?.Name;
-        return username;
-    }
-    
-    public List<string> Test()
-    {
-        string path = Path.Combine(Environment.WebRootPath, "wwwroot", "uploads", "files");
-        if (!Directory.Exists(path))
-        {
-            Directory.CreateDirectory(path);
-        }
-        List<string> fileArray = Directory.GetFiles(path).ToList();
-        List<string> files = new List<string>();
-        foreach (var file in fileArray)
-        {
-            string[] temp = file.Split('\\');
-            files.Add(temp.Last());
-        }
-        return files;
+        
     }
 }
