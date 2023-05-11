c0121124b15cb6957ced7ce045c1c3eb5dfcf879
dortwess <dortwess@gmail.com>
2023-05-11 21:57:24 +0200

    11.5.2023
    ChangeLog:
    - Prettier code

--- CABlazorApp/Data/ApplicationDbContext.cs
+++ CABlazorApp/Data/ApplicationDbContext.cs
@@ -1,5 +1,4 @@
-﻿using CABlazorApp.Models;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CABlazorApp.Data
@@ -7,8 +6,6 @@ namespace CABlazorApp.Data
     public class ApplicationDbContext : IdentityDbContext
 
     {
-        public DbSet<Users> users { get; set; }
-        public DbSet<Certificate> certificates { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
--- CABlazorApp/Pages/Generate.razor.cs
+++ CABlazorApp/Pages/Generate.razor.cs
@@ -1,38 +1,38 @@
-﻿using System.IO.Compression;
-using System.Text;
+﻿using System.Text;
 using Aspose.Zip;
 using Aspose.Zip.Saving;
 using CABlazorApp.Services;
 using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.JSInterop;
 
 namespace CABlazorApp.Pages;
 
 public partial class Generate
 {
-    //Inputs of sign
+    // Inputs of sign
     private byte[]? _encCertFile;
     private Tuple<string, byte[]>? _encDocFile;
     private string? _encPass;
 
-    //Inputs of generate 
+    // Inputs of generate 
     private string? _certName;
     private string? _certPass;
 
-    //Inputs of verify
+    // Inputs of verify
     private byte[]? _verifyFile;
     private byte[]? _verifyCert;
     private string? _verifyFileName;
 
-    //0 - encCertError, 1 - encPassError, 2 - encDocError, 3 - certNameError, 4 - certPassError, 5 - exceptionError, 6 - verifyFileError, 7 - verifyPublicKeyError, 8 - exceptionErrorOfVerify
+    // 0 - encCertError, 1 - encPassError, 2 - encDocError, 3 - certNameError, 4 - certPassError, 5 - exceptionError, 6 - verifyFileError, 7 - verifyPublicKeyError, 8 - exceptionErrorOfVerify
     private string[]? _errors = new string[9];
 
-    //States of successfull
+    // States of successfull
     private bool _encState;
     private bool _certState;
     private int _verifyState;
 
-    //Text of the required field
+    // Text of the required field
     private readonly string _required = "Toto pole je povinné.";
 
     private async Task Sign()
@@ -92,8 +92,7 @@ public partial class Generate
             {
                 String[] fileInfo = _encDocFile.Item1.Split('.');
                 String fileName = fileInfo[0];
-                String fileExtension = fileInfo[fileInfo.Length-1];
-                
+
                 string dirPath = Path.Combine(Environment.ContentRootPath, "Archive", name, "files");
                 string filePath = Path.Combine(Environment.ContentRootPath, "Archive", name, "files", _encDocFile.Item1);
                 string zipPath = Path.Combine(Environment.ContentRootPath, "Archive", name, "files", fileName + ".zip");
@@ -126,7 +125,7 @@ public partial class Generate
                 {
                     using (var archive = new Archive())
                     {
-                        archive.CreateEntry(fileName + fileExtension, filePath);
+                        archive.CreateEntry(_encDocFile.Item1, filePath);
                         archive.Save(zipFile, new ArchiveSaveOptions() { Encoding = Encoding.ASCII });
                     }
                 }
@@ -143,230 +142,285 @@ public partial class Generate
         }
     }
 
+    // Generate a certificate
     private async Task GenerateCertificate()
     {
-        var authstate = await GetAuthenticationStateAsync.GetAuthenticationStateAsync();
-        var user = authstate.User;
-        var name = user.Identity.Name;
-
+        // Get an username
+        var userName = await GetUsername();
+        
+        // Check if the certificate name isn't empty
         if (String.IsNullOrWhiteSpace(_certName))
         {
+            // Set certificate generate to un-successful
             _certState = false;
+            
+            // Set an error to required message
             _errors[3] = _required;
+            return;
         }
-        else if (String.IsNullOrWhiteSpace(_certPass))
+        
+        // Check if the certificate's password isn't empty
+        if (String.IsNullOrWhiteSpace(_certPass))
         {
+            // Set certificate generate to un-successful
             _certState = false;
+            
+            // Set the error to required message
             _errors[4] = _required;
+            return;
         }
-        else
+        
+        // Generate the certificates (private, public)
+        Tuple<byte[], byte[]> certificate = await CryptoService.Generate(_certPass);
+        
+        // Set the paths
+        string dirPath = Path.Combine(Environment.ContentRootPath, "Archive", userName, "certificates");
+        string dirPath2 = Path.Combine(Environment.ContentRootPath, "Archive", userName, "temp");
+        string zipPath = Path.Combine(Environment.ContentRootPath, "Archive", userName, "certificates", _certName + ".zip");
+        string certPath = Path.Combine(Environment.ContentRootPath, "Archive", userName, "temp", _certName + ".pfx");
+        string filePath = Path.Combine(Environment.ContentRootPath, "Archive", userName, "temp", _certName + "_public" + ".crt");
+
+        // Check if the directory doesn't exist
+        if (!Directory.Exists(dirPath))
         {
-            Tuple<byte[], byte[]> certificate = await CryptoService.Generate(_certPass);
-            if (name != null)
+            // Create the directory
+            Directory.CreateDirectory(dirPath);
+        }
+
+        // Check if the directory doesn't exist
+        if (!Directory.Exists(dirPath2))
+        {
+            // Create the directory
+            Directory.CreateDirectory(dirPath2);
+        }
+
+        // Check if the certificate is already exist
+        if (File.Exists(certPath))
+        {
+            // Delete the old certificate
+            File.Delete(certPath);
+            
+            // Create the new certificate
+            File.Create(certPath).Close();
+        }
+
+        // Check if the file is already exist
+        if (File.Exists(filePath))
+        {
+            // Delete the old file
+            File.Delete(filePath);
+            
+            // Create the new file
+            File.Create(filePath).Close();
+        }
+
+        // Check if the certificate doesn't exist
+        if (!File.Exists(certPath))
+        {
+            // Create the new certificate
+            File.Create(certPath).Close();
+        }
+
+        // Check if the file doesn't exist
+        if (!File.Exists(filePath))
+        {
+            // Create the new file
+            File.Create(filePath).Close();
+        }
+
+        // Write all bytes to the files
+        await File.WriteAllBytesAsync(certPath, certificate.Item1);
+        await File.WriteAllBytesAsync(filePath, certificate.Item2);
+
+        // Check if the zip file doesn't exist
+        if (File.Exists(zipPath))
+        {
+            File.Delete(zipPath);
+        }
+
+        // Zip the files
+        await using (var zipFile = File.Open(zipPath, FileMode.Create))
+        {
+            using (var archive = new Archive())
             {
-                string dirPath = Path.Combine(Environment.ContentRootPath, "Archive", name, "certificates");
-                string dirPath2 = Path.Combine(Environment.ContentRootPath, "Archive", name, "temp");
-                string zipPath = Path.Combine(Environment.ContentRootPath, "Archive", name, "certificates",
-                    _certName + ".zip");
-                string certPath = Path.Combine(Environment.ContentRootPath, "Archive", name, "temp",
-                    _certName + ".pfx");
-                string filePath = Path.Combine(Environment.ContentRootPath, "Archive", name, "temp",
-                    _certName + "_public" + ".crt");
-
-
-                if (!Directory.Exists(dirPath))
-                {
-                    Directory.CreateDirectory(dirPath);
-                }
-
-                if (!Directory.Exists(dirPath2))
-                {
-                    Directory.CreateDirectory(dirPath2);
-                }
-
-                if (File.Exists(certPath))
-                {
-                    File.Delete(certPath);
-                    File.Create(certPath).Close();
-                }
-
-                if (File.Exists(filePath))
-                {
-                    File.Delete(filePath);
-                    File.Create(filePath).Close();
-                }
-
-                if (!File.Exists(certPath))
-                {
-                    File.Create(certPath).Close();
-                }
-
-                if (!File.Exists(filePath))
-                {
-                    File.Create(filePath).Close();
-                }
-
-                File.WriteAllBytes(certPath, certificate.Item1);
-                File.WriteAllBytes(filePath, certificate.Item2);
-
-                if (File.Exists(zipPath))
-                {
-                    File.Delete(zipPath);
-                }
-
-                using (FileStream zipFile = File.Open(zipPath, FileMode.Create))
-                {
-                    using (var archive = new Archive())
-                    {
-                        archive.CreateEntry(_certName + ".pfx", certPath);
-                        archive.CreateEntry(_certName + "_public" + ".crt", filePath);
-                        archive.Save(zipFile, new ArchiveSaveOptions() { Encoding = Encoding.ASCII });
-                    }
-                }
-
-                _errors[3] = "";
-                _errors[4] = "";
-                _certState = true;
-
-                File.Delete(certPath);
-                File.Delete(filePath);
+                archive.CreateEntry(_certName + ".pfx", certPath);
+                archive.CreateEntry(_certName + "_public" + ".crt", filePath);
+                archive.Save(zipFile, new ArchiveSaveOptions() { Encoding = Encoding.ASCII });
             }
         }
+
+        // Clear the errors
+        _errors[3] = "";
+        _errors[4] = "";
+        
+        // Set certificate generate to successful
+        _certState = true;
+
+        // Delete the un-zipped files
+        File.Delete(certPath);
+        File.Delete(filePath);
     }
 
+    // Verify method
     private async Task Verify()
     {
-        var authstate = await GetAuthenticationStateAsync.GetAuthenticationStateAsync();
-        var user = authstate.User;
-        var name = user.Identity.Name;
-        string filePath = Path.Combine(Environment.ContentRootPath, "Archive", name, "temp", _verifyFileName);
-        string extractPath = Path.Combine(Environment.ContentRootPath, "Archive", name, "temp");
-        string[] splitted = _verifyFileName.Split('.');
+        // Get an username
+        var userName = await GetUsername();
+        
+        // Set the file path
+        var filePath = Path.Combine(Environment.ContentRootPath, "Archive", userName, "temp", _verifyFileName);
 
-        if (_verifyFile == null)
+        // Check if the file input isn't empty
+        if (_verifyFile.IsNullOrEmpty())
         {
+            // Set verify state to un-successful
             _verifyState = 0;
+            
+            // Set an error to required message
             _errors[6] = _required;
+            return;
         }
-        else if (_verifyCert == null)
+        
+        // Check if the certificate input isn't empty
+        if (_verifyCert.IsNullOrEmpty())
         {
+            // Set verify state to un-successful
             _verifyState = 0;
+            
+            // Set the error to required message
             _errors[7] = _required;
+            return;
         }
-        else
+        
+        // Check if the file is already exist
+        if (File.Exists(filePath))
         {
-            if (File.Exists(filePath))
+            // Delete the old file
+            File.Delete(filePath);
+            
+            // Create the new file & write all bytes
+            File.Create(filePath).Close();
+            await File.WriteAllBytesAsync(filePath, _verifyFile);
+        }
+
+        // Check if the file doesn't exist
+        if (!File.Exists(filePath))
+        {
+            // Create the new file & write all bytes
+            File.Create(filePath).Close();
+            await File.WriteAllBytesAsync(filePath, _verifyFile);
+        }
+
+        try
+        {
+            // Try to get a signature from alternate data stream
+            byte[] signature = Convert.FromBase64String(AlternateDataStream.ReadAds(filePath, "Signature"));
+            
+            // Verify if the signature is from certificate
+            _verifyState = await CryptoService.Verify(_verifyFile, signature, _verifyCert);
+
+            // Clear all errors
+            _errors[6] = "";
+            _errors[7] = "";
+            _errors[8] = "";
+        }
+        catch (Exception e)
+        {
+            // If ADS doesn't exist
+            if (e.Message == "Failed to open the ADS 'Signature' for '" + filePath + "'.")
             {
+                // Set an error message
+                _errors[8] = "Nepodařilo se najít vlastnost s názvem 'Signature'.";
+                
+                // Delete temp file
                 File.Delete(filePath);
-                File.Create(filePath).Close();
-                await File.WriteAllBytesAsync(filePath, _verifyFile);
+                return;
             }
 
-            if (!File.Exists(filePath))
-            {
-                File.Create(filePath).Close();
-                await File.WriteAllBytesAsync(filePath, _verifyFile);
-            }
-
-            ZipFile.ExtractToDirectory(filePath, extractPath);
-
-            string file = Path.Combine(Environment.ContentRootPath, "Archive", name, "temp", "file.txt");
-
-            try
-            {
-                byte[] signature = Convert.FromBase64String(AlternateDataStream.ReadAds(file, "Signature"));
-                _verifyState = await CryptoService.Verify(_verifyFile, signature, _verifyCert);
-
-                _errors[6] = "";
-                _errors[7] = "";
-                _errors[8] = "";
-            }
-            catch (Exception e)
-            {
-                if (e.Message == "Failed to open the ADS 'Signature' for '" + filePath + "'.")
-                {
-                    _errors[8] = "Nepodařilo se najít vlastnost s názvem 'Signature'.";
-                    File.Delete(filePath);
-                    return;
-                }
-
-                File.Delete(filePath);
-            }
+            // Delete temp file
+            File.Delete(filePath);
         }
     }
 
+    // Download method
     private async Task Download(string fileNameWithExtension, bool file)
     {
-        var authstate = await GetAuthenticationStateAsync.GetAuthenticationStateAsync();
-        var user = authstate.User;
-        var userName = user.Identity.Name;
-        string path;
-        string fileType;
-        string fileName = "";
-        
-        if (file)
-        {
-            List<string> splitedFileName = fileNameWithExtension.Split('.').ToList();
-            splitedFileName.RemoveAt(splitedFileName.Count - 1);
-            
-            foreach (var item in splitedFileName)
-            {
-                if (fileName == "")
-                {
-                    fileName += item;
-                }
-                else
-                {
-                    fileName += "." + item;
-                }
-            }
-            
-            path = Path.Combine(Environment.ContentRootPath, "Archive", userName, "files", fileName + ".zip");
-            fileType = "application/zip";
-        }
-        else
-        {
-            path = Path.Combine(Environment.ContentRootPath, "Archive", userName, "certificates", fileNameWithExtension);
-            fileType = "application/x-pkcs12";
-        }
+        // Get an username
+        var userName = await GetUsername();
 
-        if (fileName == "")
-        {
-            await JsRuntime.InvokeVoidAsync("Download", fileNameWithExtension, Convert.ToBase64String(File.ReadAllBytes(path)));
-        }
-        else
-        {
-            await JsRuntime.InvokeVoidAsync("Download", fileName, fileType, Convert.ToBase64String(File.ReadAllBytes(path)));
-        }
+        // Get a file type
+        var fileType = MimeTypes.GetContentType(fileNameWithExtension);
         
+        // Split the file name
+        var splitFileName = fileNameWithExtension.Split(".");
+        
+        // Get the file name without an extension
+        var fileName = splitFileName.Take(splitFileName.Length - 1);
+
+        // Get a path of file
+        var path = file ? Path.Combine(Environment.ContentRootPath, "Archive", userName ?? throw new InvalidOperationException(), "files", fileName + ".zip") : Path.Combine(Environment.ContentRootPath, "Archive", userName, "certificates", fileNameWithExtension);
+        
+        // Execute JS download
+        await JsRuntime.InvokeVoidAsync("Download", fileName, fileType, Convert.ToBase64String(await File.ReadAllBytesAsync(path)));
     }
     
+    // Methods after input change event
     private async Task OnEncryptDocumentFile(InputFileChangeEventArgs obj)
     {
+        // Create a memory stream
         await using MemoryStream stream = new MemoryStream();
+        
+        // Copy object to stream
         await obj.File.OpenReadStream().CopyToAsync(stream);
+        
+        // Save the stream
         _encDocFile = new Tuple<string, byte[]>(obj.File.Name, stream.ToArray());
     }
 
     private async Task OnEncryptCertFile(InputFileChangeEventArgs obj)
     {
+        // Create a memory stream
         await using MemoryStream stream = new MemoryStream();
+        
+        // Copy object to stream
         await obj.File.OpenReadStream().CopyToAsync(stream);
+        
+        // Save the stream
         _encCertFile = stream.ToArray();
     }
 
     private async Task OnVerifyDocumentFile(InputFileChangeEventArgs obj)
     {
+        // Create a memory stream
         await using MemoryStream stream = new MemoryStream();
+        
+        // Copy object to stream
         await obj.File.OpenReadStream().CopyToAsync(stream);
+        
+        // Save the stream and a file name
         _verifyFile = stream.ToArray();
         _verifyFileName = obj.File.Name;
     }
-
+    
     private async Task OnVerifyCert(InputFileChangeEventArgs obj)
     {
+        // Create a memory stream
         await using MemoryStream stream = new MemoryStream();
+        
+        // Copy object to stream
         await obj.File.OpenReadStream().CopyToAsync(stream);
+        
+        // Save the stream
         _verifyCert = stream.ToArray();
+    }
+
+    // Get a username method
+    private async Task<string?> GetUsername()
+    {
+        // Get auth state
+        var authState = await GetAuthenticationStateAsync.GetAuthenticationStateAsync();
+        
+        // Return username as string
+        return authState.User.Identity?.Name;
     }
 }
